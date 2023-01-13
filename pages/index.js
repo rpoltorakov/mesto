4715eab026dfdc7ccdc9860ec04676bcb069780e
rpoltorakov <rpoltorakov@gmail.com>
2023-01-14 01:53:17 +0300

    final polish

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 let editProfileButton = document.querySelector('.profile__edit-button')
 function showPopup() {
-  document.querySelector('.popup').classList.add('popup_opened')
-  document.querySelector('.popup__title').value = document.querySelector('.profile__title').innerHTML
-  document.querySelector('.popup__subtitle').value = document.querySelector('.profile__subtitle').innerHTML
+  let popup = document.querySelector('.popup')
+  popup.classList.add('popup_opened')
+  let popupTitle = document.querySelector('.popup__title')
+  let profileTitle = document.querySelector('.profile__title')
+  popupTitle.value = profileTitle.innerHTML
+  let popupSubtitle = document.querySelector('.popup__subtitle')
+  let profileSubtitle = document.querySelector('.profile__subtitle')
+  popupSubtitle.value = profileSubtitle.innerHTML
 }
 editProfileButton.addEventListener('click', showPopup)
 let closePopupButton = document.querySelector('.popup__close-button')
 function closePopup() {
-  document.querySelector('.popup').classList.remove('popup_opened')
+  let popup = document.querySelector('.popup')
+  popup.classList.remove('popup_opened')
 }
 closePopupButton.addEventListener('click', closePopup)
 let formElement = document.querySelector('.popup__form')
