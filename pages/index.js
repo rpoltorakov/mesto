f7747cab81e0dd2791115752758a1b1703d7e49c
rpoltorakov <rpoltorakov@gmail.com>
2023-01-15 01:07:38 +0300

    after 1 review

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,34 @@
-let editProfileButton = document.querySelector('.profile__edit-button')
+let profileEditButton = document.querySelector('.profile__edit-button')
+let popup = document.querySelector('.popup')
+let profileTitle = document.querySelector('.profile__title')
+let profileSubtitle = document.querySelector('.profile__subtitle')
+let popupTitle = document.querySelector('.popup__title')
+let popupSubtitle = document.querySelector('.popup__subtitle')
+
 function showPopup() {
-  let popup = document.querySelector('.popup')
   popup.classList.add('popup_opened')
-  let popupTitle = document.querySelector('.popup__title')
-  let profileTitle = document.querySelector('.profile__title')
-  popupTitle.value = profileTitle.innerHTML
-  let popupSubtitle = document.querySelector('.popup__subtitle')
-  let profileSubtitle = document.querySelector('.profile__subtitle')
-  popupSubtitle.value = profileSubtitle.innerHTML
+  popupTitle.value = profileTitle.textContent
+  popupSubtitle.value = profileSubtitle.textContent
 }
-editProfileButton.addEventListener('click', showPopup)
+
 let closePopupButton = document.querySelector('.popup__close-button')
+
 function closePopup() {
   let popup = document.querySelector('.popup')
   popup.classList.remove('popup_opened')
 }
-closePopupButton.addEventListener('click', closePopup)
+
 let formElement = document.querySelector('.popup__form')
 let nameInput = document.querySelector('.popup__title')
 let jobInput = document.querySelector('.popup__subtitle')
+
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  let nameInpitValue = nameInput.value
-  let jobInputValue = jobInput.value
-  let profileTitle = document.querySelector('.profile__title')
-  let profileSubtitle = document.querySelector('.profile__subtitle')
-  profileTitle.textContent = nameInpitValue
-  profileSubtitle.textContent = jobInputValue
+  profileTitle.textContent = nameInput.value
+  profileSubtitle.textContent = jobInput.value
+  closePopup()
 }
+
+profileEditButton.addEventListener('click', showPopup)
+closePopupButton.addEventListener('click', closePopup)
 formElement.addEventListener('submit', handleFormSubmit); 
